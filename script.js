10b29e465d7f4f113de4cf855a3a5873996efeb2
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-25 11:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
    let radi=document.getElementById("radius").value;
             let vol= document.getElementById("volume");
             let cal=(4/3)*(22/7)*(radi*radi*radi);
-            console.log(radi.value);
+           // console.log(radi.value);
               vol.value=cal;
 } 
 
